a52b9f5b19eb6abdb87161e81efbcc6257a59647
jomarque <jomarque@redhat.com>
2024-06-10 23:10:29 +0200

    Modified app02 image.

--- devconf2024/04_deploy_applications/devconf2024-application-01/src/gpt3-bot/app.py
+++ devconf2024/04_deploy_applications/devconf2024-application-01/src/gpt3-bot/app.py
@@ -11,10 +11,10 @@ def open_file(filepath):
         return infile.read()
 
 # Create an openaiapikey.txt file and save your api key.
-openai.api_key = "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"
+openai.api_key = "sk-proj-88rXZOWLDp3TLv7pLyz4T3BlbkFJCRZEIMiNp3GuXYXu0Dsp"
 
 
-def bot(prompt, engine='text-davinci-002', temp=0.9, top_p=1.0, tokens=1000, freq_pen=0.0, pres_pen=0.5, stop=['<<END>>']):
+def bot(prompt, engine='gpt-3.5-turbo-instruct', temp=0.9, top_p=1.0, tokens=1000, freq_pen=0.0, pres_pen=0.5, stop=['<<END>>']):
     max_retry = 1
     retry = 0
     while True:
